cd3d93a8ec2ee888b212c19d0a9ae6bed07fd30d
Daniel Rose <danielrose@member.fsf.org>
2021-03-12 19:37:53 -0500

    Add z critical values

--- src/formula/intervals.rs
+++ src/formula/intervals.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use std::collections::HashMap;
 
-fn get_p_value(z_score: u64) -> f64 {
-    match z_score {
+fn get_z_crit(c_level: u64) -> f64 {
+    match c_level {
         90 => 1.6448536270,
         95 => 1.9599639845,
         99 => 2.5758293035,
@@ -10,13 +10,18 @@ fn get_p_value(z_score: u64) -> f64 {
     }
 }
 
+fn get_t_crit(c_level: u64, df: u64) -> f64 {
+    0.0
+}
+
 pub fn get_z_interval(sigma: u64, xbar: u64, n: u64, c_level: u64) -> Result<String> {
-    let z_score = get_p_value(c_level);
+    let z_score = get_z_crit(c_level);
 
     Ok(format!(
-        "Upper: {}\nLower: {}",
+        "Upper: {}\nLower: {}\nME: {}",
         (xbar as f64 + (z_score * sigma as f64 / (n as f64).sqrt())),
-        (xbar as f64 - (z_score * (sigma as f64 / (n as f64).sqrt())))
+        (xbar as f64 - (z_score * (sigma as f64 / (n as f64).sqrt()))),
+        (z_score * (sigma as f64 / (n as f64).sqrt()))
     ))
 }
 
